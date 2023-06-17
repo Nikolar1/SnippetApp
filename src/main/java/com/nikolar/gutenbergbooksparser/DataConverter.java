118d3c1051c9a1ef75d32fae291f80d9f421c900
Nikola Radojcic <nikolajazak@gmail.com>
2023-06-17 15:15:32 +0200

    Created a parser for Gutenberg books and converter to arff files

---
+++ src/main/java/com/nikolar/gutenbergbooksparser/DataConverter.java
@@ -0,0 +1,96 @@
+package com.nikolar.gutenbergbooksparser;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class DataConverter {
+    private String outputFileName;
+
+    public DataConverter(String outputFileName){
+        this.outputFileName = outputFileName;
+    }
+
+    private void writeAuthor(DataParser dp, BufferedWriter output, int length, String[] books) throws IOException {
+        for (int j = 0; j<length; j++) {
+            dp.SetFile(books[j]);
+            LinkedList<String> parsedText = dp.Parse();
+            for (int i = 0; i < parsedText.size(); i++) {
+                output.append(parsedText.get(i));
+                output.newLine();
+            }
+        }
+    }
+
+    private void writeArffHeader(BufferedWriter output, String name, String[] attributes, String[] classes) throws IOException {
+        output.append("@RELATION " + name);
+        output.newLine();
+        for (int i = 0; i<attributes.length; i++) {
+            output.newLine();
+            output.append("@attribute " + attributes[i]);
+            output.newLine();
+        }
+        String classLine = "@attribute class {";
+        for (int i = 0; i<classes.length; i++) {
+            classLine += classes[i];
+            if (i != classes.length-1){
+                classLine+= ",";
+            }
+        }
+        output.append(classLine + "}");
+        output.newLine();
+        output.newLine();
+        output.append("@data");
+        output.newLine();
+    }
+
+    public String[] toArffTrainingTest(){
+        DataParser dp = new DataParser(SourceLists.TrainingPaths[0], SourceLists.Authors[0]);
+        //Passes through pre-defined list of books for training by author and adds them to {outputFileName}-training.arff
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(outputFileName + "-training.arff", false));
+            String[] attributes = {"Text string"};
+            writeArffHeader(output, "writer-training", attributes,SourceLists.Authors);
+            writeAuthor(dp, output, SourceLists.EbersGeorgTraining.length, SourceLists.EbersGeorgTraining);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[1], SourceLists.Authors[1]);
+            writeAuthor(dp, output, SourceLists.TolstoyLeoTraining.length, SourceLists.TolstoyLeoTraining);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[2], SourceLists.Authors[2]);
+            writeAuthor(dp, output, SourceLists.GeorgeOSmithTraining.length, SourceLists.GeorgeOSmithTraining);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[3], SourceLists.Authors[3]);
+            writeAuthor(dp, output, SourceLists.StrangHerbertTraining.length, SourceLists.StrangHerbertTraining);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[4], SourceLists.Authors[4]);
+            writeAuthor(dp, output, SourceLists.WebsterFrankVTraining.length, SourceLists.WebsterFrankVTraining);
+            output.close();
+        }catch (IOException e){
+            System.out.println("Error while writing arff training file");
+            e.printStackTrace();
+            return null;
+        }
+
+        //Passes through pre-defined list of books for testing by author and adds them to {outputFileName}-test.arff
+        try {
+            BufferedWriter output = new BufferedWriter(new FileWriter(outputFileName + "-test.arff", false));
+            String[] attributes = {"Text string"};
+            writeArffHeader(output, "writer-test", attributes,SourceLists.Authors);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[0] + "Test/", SourceLists.Authors[0]);
+            writeAuthor(dp, output, SourceLists.EbersGeorgTest.length, SourceLists.EbersGeorgTest);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[1] + "Test/", SourceLists.Authors[1]);
+            writeAuthor(dp, output, SourceLists.TolstoyLeoTest.length, SourceLists.TolstoyLeoTest);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[2] + "Test/", SourceLists.Authors[2]);
+            writeAuthor(dp, output, SourceLists.GeorgeOSmithTest.length, SourceLists.GeorgeOSmithTest);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[3] + "Test/", SourceLists.Authors[3]);
+            writeAuthor(dp, output, SourceLists.StrangHerbertTest.length, SourceLists.StrangHerbertTest);
+            dp.ChangeAuthor(SourceLists.TrainingPaths[4] + "Test/", SourceLists.Authors[4]);
+            writeAuthor(dp, output, SourceLists.WebsterFrankVTest.length, SourceLists.WebsterFrankVTest);
+            output.close();
+        }catch (IOException e){
+            System.out.println("Error while writing arff test file");
+            e.printStackTrace();
+            return null;
+        }
+
+        String[] rez = {outputFileName + "-training.arff", outputFileName + "-test.arff"};
+        return rez;
+    }
+}
---
+++ src/main/java/com/nikolar/gutenbergbooksparser/DataParser.java
@@ -0,0 +1,119 @@
+package com.nikolar.gutenbergbooksparser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataParser {
+    //Number of lines to delete from the begining of the book
+    private static int firstLinesToDelete = 35;
+    //Number of lines to delete from the end of the book
+    private static int lastLinesToDelete = 400;
+    //Name of the current book
+    private String filename;
+    //Route to the book folder
+    private String filepath;
+    //Book Author
+    private String author;
+    public DataParser(String filepath, String author){
+        this.filepath = filepath;
+        this.author = author;
+    }
+
+    //Change of autor when going to the next one
+    public void ChangeAuthor(String filepath, String author){
+        this.filepath = filepath;
+        this.author = author;
+    }
+
+    //Change of book when going to the next one
+    public void SetFile(String filename){
+        this.filename = filepath + filename;
+    }
+
+    private LinkedList<String> removeFirstAndLast(LinkedList<String> inputText){
+        //Deletes excess lines from the beggining of the book
+        for (int i = 0; i<firstLinesToDelete; i++){
+            inputText.removeFirst();
+        }
+        //Deletes lines from the end of the book but checks if there are enough lines to avoid an error
+        int size = Math.max(inputText.size() - lastLinesToDelete,0);
+        if (size!=0) {
+            for (int i = inputText.size(); i > size; i--) {
+                inputText.remove(i - 1);
+            }
+        }
+        return inputText;
+    }
+
+    private String processLine(String text){
+        if (text == ""){
+            return "";
+        }
+        //Skip snippets that contain word chapter
+        if (text.contains("CHAPTER")){
+            return "";
+        }
+        //If the current text does not contain any letters, skip it
+        String regex = ".*[a-zA-Z].*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcherText = pattern.matcher(text);
+        if (!matcherText.matches()){
+            return "";
+        }
+
+        //Escapes all ' characters, because they represent end of an instance in .arff files
+        text = text.replace("\'", "\\\'");
+        //Standardizes all ' characters because two types are used in original books
+        //They would skew the data otherwise
+        text = text.replace("’", "\\\'");
+        text = "\'" + text + "\'" +  ", " + author;
+        return text;
+    }
+
+    public LinkedList<String> Parse(){
+        if (filename == null){
+            return null;
+        }
+        LinkedList<String> inputText = new LinkedList<String>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            String line;
+            //Reads all lines from a file
+            while((line = in.readLine()) != null)
+            {
+                inputText.add(line);
+            }
+            in.close();
+            inputText = removeFirstAndLast(inputText);
+            String text = "";
+            LinkedList<String> parsedText = new LinkedList<>();
+            //Passes through lines to connect and proccess them
+            for (int i = 0; i<inputText.size()-1; i++){
+                line = inputText.get(i);
+                //Forms a snippet if it reaches the end
+                if (line.trim().isEmpty()){
+                    text = processLine(text);
+                    if (text != "") {
+                        parsedText.add(text);
+                        text = "";
+                    }
+                }
+                if (text == ""){
+                    text = line;
+                }else {
+                    //Adds the line to current text and continues the search
+                    text = text + " " + line;
+                }
+            }
+            return parsedText;
+        }catch (IOException e) {
+            System.out.println("Error while reading file:");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
